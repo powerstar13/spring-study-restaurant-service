9c9341581217a6751d5c7af28dc5f0910a4cba84
홍준성 <ghdwnstjd13@gmail.com>
2022-09-02 22:09:09 +0900

    git init > New Project

---
+++ src/main/java/spring/study/restaurant/SpringStduyRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package spring.study.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStduyRestaurantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStduyRestaurantServiceApplication.class, args);
+    }
+
+}
